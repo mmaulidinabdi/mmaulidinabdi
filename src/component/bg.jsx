2c1b3a896e2e52728b818f1b8d9a7245e1e60caf
Maulidin <mmaulidinabdi@mail.com>
2025-08-08 10:51:28 +0800

    882025

--- src/component/bg.jsx
+++ src/component/bg.jsx
@@ -107,7 +107,7 @@ const TechRainBackground = ({
   return (
     <canvas
       ref={canvasRef}
-      className="fixed top-0 left-0 w-full h-full -z-10 pointer-events-none bg-black"
+      className="absolute top-0 left-0 w-full h-full -z-10 pointer-events-none bg-black"
       style={{ opacity }}
     />
   );
